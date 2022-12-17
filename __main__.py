526bb01e4d05b5e78f7090479847b383f75319cb
Mohammad Ayub <asimayub81@gmail.com>
2022-12-17 13:56:22 +0000

    added postgres sg

--- __main__.py
+++ __main__.py
@@ -7,12 +7,17 @@ from utils import conf, env
 
 # Create vpc
 vpc_config = conf.get_object("vpc")
-vpc = awsx.ec2.Vpc(f"{env}-ecs-vpc", cidr_block=vpc_config.get("vpc_cidr"))
+vpc = awsx.ec2.Vpc(
+    f"{env}-ecs-vpc",
+    cidr_block=vpc_config.get("vpc_cidr"),
+    enable_dns_hostnames=True,
+    enable_dns_support=True,
+)
 
 # Creat security group
 security_group = aws.ec2.SecurityGroup(
     f"{env}-ecs-sg",
-    description="Allow all outbound traffic",
+    description="Allow all outbound and inbound traffic",
     vpc_id=vpc.vpc_id,
     egress=[
         aws.ec2.SecurityGroupEgressArgs(
@@ -29,8 +34,31 @@ security_group = aws.ec2.SecurityGroup(
             protocol="tcp",
             cidr_blocks=["0.0.0.0/0"],
         )
+    ],
+)
+# Create services security-group
+postgres_sg = aws.ec2.SecurityGroup(
+    f"{env}-postgres-sg",
+    description="Allow outbound traffic",
+    vpc_id=vpc.vpc_id,
+    egress=[
+        aws.ec2.SecurityGroupEgressArgs(
+            from_port=0,
+            to_port=0,
+            protocol=-1,
+            cidr_blocks=["0.0.0.0/0"]
+        )
+    ],
+    ingress=[
+        aws.ec2.SecurityGroupIngressArgs(
+            from_port=5432,
+            to_port=5432,
+            protocol="tcp",
+            cidr_blocks=[f"{vpc_config.get('vpc_cidr')}"]
+        )
     ]
 )
+
 # Creat ECS cluster
 cluster = aws.ecs.Cluster(
     f"{env}-ecs-cluster",
@@ -44,7 +72,7 @@ alb = awsx.lb.ApplicationLoadBalancer(
     subnet_ids=vpc.public_subnet_ids,
 )
 
-# Create ECS service
+# Create Nginx ECS service
 nginx_service = awsx.ecs.FargateService(
     f"{env}-nginx-service",
     cluster=cluster.arn,
@@ -59,14 +87,16 @@ nginx_service = awsx.ecs.FargateService(
             cpu=512,
             memory=128,
             essential=True,
-            port_mappings=[awsx.ecs.TaskDefinitionPortMappingArgs(
-                target_group=alb.default_target_group,
-            )]
+            port_mappings=[
+                awsx.ecs.TaskDefinitionPortMappingArgs(
+                    target_group=alb.default_target_group,
+                )
+            ],
         )
-    )
+    ),
 )
-# # Export the vpc_id
-pulumi.export("vpc_id", vpc.id)
-# # Export the vpc cidr
-pulumi.export("vpc_cidr", vpc.cidr_block)
+
+# Export the vpc_id
+pulumi.export("vpc_id", vpc.vpc_id)
+
 pulumi.export("url", alb.load_balancer.dns_name)
