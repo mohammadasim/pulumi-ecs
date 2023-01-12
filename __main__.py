963a867be08100ee6230c5ef82f583cfd89f1356
Mohammad Asim Ayub <mohammad.ayub@gmail.com>
2023-01-12 11:26:35 +0000

    fixed port range

--- __main__.py
+++ __main__.py
@@ -33,7 +33,7 @@ security_group = aws.ec2.SecurityGroup(
     ingress=[
         aws.ec2.SecurityGroupIngressArgs(
             from_port=80,
-            to_port=443,
+            to_port=80,
             protocol="tcp",
             cidr_blocks=["0.0.0.0/0"],
         )
